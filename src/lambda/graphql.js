1d333f67163e3695ba3ca6468fc7b0b5f02c5ce9
Carlos Medina <carlos.w.med@gmail.com>
2019-04-06 13:31:17 -0400

    chore: move index into graphql lambda

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -1,23 +1,34 @@
+require( 'dotenv' ).config()
 const { ApolloServer, gql } = require( 'apollo-server-lambda' );
+const typeDefs = require( '../schema' );
+const GhibliAPI = require( '../dataSource' );
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    getFilms: async ( _source, _args, { dataSources } ) =>
+      await dataSources.ghibliAPI.getFilms(),
+    getPeople: ( _source, _args, { dataSources } ) =>
+      dataSources.ghibliAPI.getPeople(),
+    getLocations: ( _source, _args, { dataSources } ) =>
+      dataSources.ghibliAPI.getLocations(),
+    getSpecies: ( _source, _args, { dataSources } ) =>
+      dataSources.ghibliAPI.getSpecies(),
+    getVehicles: ( _source, _args, { dataSources } ) =>
+      dataSources.ghibliAPI.getVehicles(),
   },
 };
 
 const server = new ApolloServer( {
   typeDefs,
   resolvers,
-  playground: true
+  dataSources: () => ( {
+    ghibliAPI: new GhibliAPI(),
+  } ),
+  engine: {
+    apiKey: process.env.ENGINE_API_KEY,
+  },
+  playground: true,
+  introspection: true
 } );
 
 exports.handler = server.createHandler();
